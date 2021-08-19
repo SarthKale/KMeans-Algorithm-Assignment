d73b517022f118b6d4d350aeffc2444644a5e641
SarthKale <sarthak.kale12@gmail.com>
2021-08-19 14:16:22 +0530

    Comments added

--- solution.py
+++ solution.py
@@ -18,28 +18,40 @@ def get_k_means(user_feature_map, num_features_per_user, k):
     if k < 1:
         print([])
         return []
+    # Creating the list of Centroid class objects and
+    # a value point specific dataset.
     centroid_values = [Centroid(user_feature_map[value])
                        for value in inital_centroid_users]
     dataset = create_dataset(user_feature_map)
     old_centroids = centroid_values.copy()
+    # Iterate over centroid values, with max iterations = 100.
     for i in range(100):
+        # calculating the distance of points from centroids.
         distance = []
         for row in dataset:
             tmp = []
             for centroid in centroid_values:
                 tmp.append(calculate_distance(row, centroid.location))
             distance.append(tmp)
+        # Updating centroid values(location).
         if len(centroid_values) > 1:
+            # Allocating clustters.
             classes = []
             for row in distance:
                 classes.append(row.index(min(*row)))
+            # Calculating mean values.
             centroid_values = calculate_mean(dataset, centroid_values, classes)
         else:
+            # Calculating mean values.
             centroid_values = calculate_mean(dataset, centroid_values)
         if centroid_values == old_centroids and i > 10:
+            # Old and new centroid values matched and
+            # a total of 10 iterations is complete.
             break
         else:
+            # Allocating new unmatched centroid values as old centroid values.
             old_centroids = centroid_values
+    # Rounding the output values to fourth decimal.
     centroid_values = [[round(value, 3) for value in centroid.location]
                        for centroid in centroid_values]
     print(centroid_values)
